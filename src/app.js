4dd7106aaa01d5d6f3dc2ec43bc96d8405fc4542
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-08-18 18:22:58 +0000

    random card working on refresh

--- src/app.js
+++ src/app.js
@@ -7,5 +7,59 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let cardNumberElement = document.getElementById("cardNumber");
+  let cardSuitTElement = document.getElementById("cardSuitT");
+  let cardSuitBElement = document.getElementById("cardSuitB");
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  let randomSuit = Math.floor(Math.random() * 4) + 1;
+  let randomNumberString;
+  let randomSuitString;
+  let randomSuitColor;
+
+  // asign number or picture card
+  switch (randomNumber) {
+    case 1:
+      randomNumberString = "A";
+      break;
+    case 11:
+      randomNumberString = "J";
+      break;
+    case 12:
+      randomNumberString = "Q";
+      break;
+    case 13:
+      randomNumberString = "K";
+      break;
+    default:
+      randomNumberString = randomNumber;
+  }
+  console.log(randomNumberString);
+
+  // asign suit
+  switch (randomSuit) {
+    case 1:
+      randomSuitString = "♦";
+      randomSuitColor = "red";
+      break;
+    case 2:
+      randomSuitString = "♥";
+      randomSuitColor = "red";
+      break;
+    case 3:
+      randomSuitString = "♠";
+      randomSuitColor = "black";
+      break;
+    case 4:
+      randomSuitString = "♣";
+      randomSuitColor = "black";
+      break;
+  }
+  console.log(randomSuitString);
+
+  cardNumberElement.innerHTML = randomNumberString;
+  cardSuitTElement.innerHTML = randomSuitString;
+  cardSuitBElement.innerHTML = randomSuitString;
+  cardSuitTElement.style.color = randomSuitColor;
+  cardSuitBElement.style.color = randomSuitColor;
 };
